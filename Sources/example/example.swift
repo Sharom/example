dd42fc36a5ac5fe593744886fa1d8f00679ae55f
Roman Shamritskiy <roman@litgroup.ru>
2021-11-09 22:15:23 +0300

    Initial commit

---
+++ Sources/example/example.swift
@@ -0,0 +1,6 @@
+public struct example {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
---
+++ Tests/exampleTests/exampleTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import example
+
+final class exampleTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(example().text, "Hello, World!")
+    }
+}
